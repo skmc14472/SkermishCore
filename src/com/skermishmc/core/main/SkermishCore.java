bef9b944dd67d32305991f27ecf046e8c044b1b3
Distracter <contactvirizin@gmail.com>
2018-12-07 20:27:01 -0500

    Fixed UUID problem in SkermishCore.java

--- src/com/skermishmc/core/main/SkermishCore.java
+++ src/com/skermishmc/core/main/SkermishCore.java
@@ -5,10 +5,7 @@ import java.util.logging.Logger;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
-<<<<<<< HEAD
 import com.skermishmc.core.commands.CoreCommand;
-=======
->>>>>>> 7cf3c1c58f421252e42cc5514f228af1c0792842
 import com.skermishmc.core.utils.Config;
 
 import net.milkbowl.vault.economy.Economy;
@@ -17,21 +14,15 @@ public class SkermishCore extends JavaPlugin {
 
 	private static Economy econ = null;
 	private static final Logger log = Logger.getLogger("Minecraft");
-<<<<<<< HEAD
 	private static Config cfg;
-=======
->>>>>>> 7cf3c1c58f421252e42cc5514f228af1c0792842
 
 	public void onEnable() {
 
 		Config config = new Config(this);
 
 		config.initializeConfig();
-<<<<<<< HEAD
 		
 		setupCommands();
-=======
->>>>>>> 7cf3c1c58f421252e42cc5514f228af1c0792842
 
 		if (!setupEconomy()) {
 			log.severe(String.format("[%s] - Disabled due to no Vault dependency found!", getDescription().getName()));
@@ -63,13 +54,9 @@ public class SkermishCore extends JavaPlugin {
 	public static Economy getEconomy() {
 		return econ;
 	}
-<<<<<<< HEAD
 	
 	private void setupCommands() {
 		getCommand("core").setExecutor(new CoreCommand(cfg));
 	}
 	
-=======
-
->>>>>>> 7cf3c1c58f421252e42cc5514f228af1c0792842
 }
